33454a7ca30eadf8a68072053ee8aa15946b0d0a
Nathan Tino <tinonw01@gmail.com>
2023-02-09 13:11:56 -0800

    Add basic CRUD controllers

---
+++ BackEnd/BackEnd/Controllers/OrderLinesController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NathansStuff.Data;
+using NathansStuff.Models;
+
+namespace NathansStuffWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderLinesController : ControllerBase
+    {
+        private readonly NathansStuffContext _context;
+
+        public OrderLinesController(NathansStuffContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrderLines
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderLine>>> GetOrderLines()
+        {
+            if (_context.OrderLines == null)
+            {
+                return NotFound();
+            }
+            return await _context.OrderLines.ToListAsync();
+        }
+
+        // GET: api/OrderLines/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderLine>> GetOrderLine(int id)
+        {
+            if (_context.OrderLines == null)
+            {
+                return NotFound();
+            }
+            var orderLine = await _context.OrderLines.FindAsync(id);
+
+            if (orderLine == null)
+            {
+                return NotFound();
+            }
+
+            return orderLine;
+        }
+
+        // PUT: api/OrderLines/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderLine(int id, OrderLine orderLine)
+        {
+            if (id != orderLine.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orderLine).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderLineExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OrderLines
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<OrderLine>> PostOrderLine(OrderLine orderLine)
+        {
+            if (_context.OrderLines == null)
+            {
+                return Problem("Entity set 'NathansStuffContext.OrderLines'  is null.");
+            }
+            _context.OrderLines.Add(orderLine);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrderLine", new { id = orderLine.Id }, orderLine);
+        }
+
+        // DELETE: api/OrderLines/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderLine(int id)
+        {
+            if (_context.OrderLines == null)
+            {
+                return NotFound();
+            }
+            var orderLine = await _context.OrderLines.FindAsync(id);
+            if (orderLine == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderLines.Remove(orderLine);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderLineExists(int id)
+        {
+            return (_context.OrderLines?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
